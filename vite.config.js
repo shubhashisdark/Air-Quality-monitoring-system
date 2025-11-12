4dcf9dd580d894641cf88a67a9ffa640b4a93029
Shubhashis Baral <shubhashisbaral@gmail.com>
2025-11-12 16:41:12 +0530

    Move package.json to root for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'  // <-- Add this line
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'], // move react libraries to separate chunk
+          vendor: ['axios', 'chart.js'], // example: move other libs if needed
+        },
+      },
+    },
+  },
 });
